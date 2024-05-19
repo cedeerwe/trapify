fbc5de0b53da4ed10772073e836480439d2bd9ac
cd <cdominik@gmail.com>
2024-05-20 00:02:42 +0200

    MVP using Comfy

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,243 @@
-use egui_macroquad::egui;
-use macroquad::prelude::*;
+use comfy::{egui::ProgressBar, *};
+use trapify::*;
 
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "Trapify".to_owned(),
-        window_width: 1920,
-        window_height: 1080,
-        ..Default::default()
+simple_game!("Trapify", GameState, setup, update);
+
+pub struct GameState {
+    enemies: Vec<Enemy>,
+    enemy_spawner: EnemySpawner,
+    player: Player,
+    is_paused: bool,
+    is_game_over: bool,
+}
+
+impl GameState {
+    pub fn new(_c: &EngineState) -> Self {
+        Self {
+            enemy_spawner: EnemySpawner {
+                timer: Timer::from_seconds(1., true),
+                maximum_hp: 10.,
+                speed: 1.,
+                damage: 3.,
+            },
+            enemies: vec![],
+            player: Player {
+                hp: HitPoints::new_full(100.),
+            },
+            is_paused: false,
+            is_game_over: false,
+        }
     }
 }
 
-#[macroquad::main(window_conf)]
-async fn main() {
-    loop {
-        clear_background(RED);
+pub struct EnemySpawner {
+    timer: Timer,
+    maximum_hp: f32,
+    speed: f32,
+    damage: f32,
+}
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+impl EnemySpawner {
+    pub fn spawn(&self) -> Enemy {
+        let size = 0.5;
+        Enemy {
+            hp: HitPoints::new_full(self.maximum_hp),
+            speed: self.speed,
+            damage: self.damage,
+            position: Vec2::new(
+                tile_map::x_min() - size / 2.,
+                tile_map::y_into_absolute_mid(rand() % tile_map::ROWS),
+            ),
+            size,
+        }
+    }
+}
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+pub struct Enemy {
+    hp: HitPoints,
+    speed: f32,
+    damage: f32,
+    position: Vec2,
+    size: f32,
+}
 
-        egui_macroquad::ui(|ctx| {
-            egui::panel::TopBottomPanel::bottom("spreadsheet")
-                .exact_height(500.)
-                .show(ctx, |ui| ui.label("Hello there"));
+impl Enemy {
+    pub fn move_and_deal_damage(&mut self, delta_secs: f32) -> Option<f32> {
+        self.position.x += self.speed * delta_secs;
+        match self.position.x >= tile_map::x_max() {
+            true => Some(self.damage),
+            false => None,
+        }
+    }
+
+    pub fn show(&self) {
+        // resolves the case at the beginning of the map, when the whole square isn't seen
+        let x_min = tile_map::x_min().max(self.position.x - 0.5 * self.size);
+        let x_max = self.position.x + 0.5 * self.size;
+        let x_mid = (x_min + x_max) * 0.5;
+        let x_size = x_max - x_min;
+
+        // life-bar
+        let y_min = self.position.y - 0.5 * self.size;
+        let y_max = y_min + self.size * self.hp.remaining_fraction();
+        let y_mid = (y_min + y_max) * 0.5;
+        let y_size = y_max - y_min;
+
+        draw_rect(
+            Vec2::new(x_mid, self.position.y),
+            Vec2::new(x_size, self.size),
+            RED,
+            1,
+        );
+        draw_rect(
+            Vec2::new(x_mid, y_mid),
+            Vec2::new(x_size, y_size),
+            DARKGREEN,
+            2,
+        );
+    }
+}
+
+pub struct HitPoints {
+    maximum: f32,
+    current: f32,
+}
+
+impl HitPoints {
+    pub fn new_full(maximum: f32) -> Self {
+        Self {
+            maximum,
+            current: maximum,
+        }
+    }
+
+    pub fn remaining_fraction(&self) -> f32 {
+        self.current / self.maximum
+    }
+
+    pub fn take_damage_and_die(&mut self, damage: f32) -> bool {
+        self.current -= damage;
+        self.current <= 0.
+    }
+
+    pub fn reset(&mut self) {
+        self.current = self.maximum;
+    }
+}
+
+pub struct Player {
+    hp: HitPoints,
+}
+
+fn setup(_state: &mut GameState, _c: &mut EngineContext) {}
+
+fn update(state: &mut GameState, _c: &mut EngineContext) {
+    let delta = delta();
+    clear_background(LIGHTGRAY);
+
+    tile_map::draw();
+
+    if !state.is_paused {
+        // Move the enemies
+        state.enemies.retain_mut(|enemy| {
+            if let Some(damage) = enemy.move_and_deal_damage(delta) {
+                if state.player.hp.take_damage_and_die(damage) {
+                    state.is_paused = true;
+                    state.is_game_over = true;
+                };
+                return false;
+            }
+            true
         });
 
-        egui_macroquad::draw();
-
-        next_frame().await
+        // Spawn new enemies
+        state.enemy_spawner.timer.tick_secs(delta);
+        if state.enemy_spawner.timer.just_finished() {
+            state.enemies.push(state.enemy_spawner.spawn())
+        }
     }
+
+    // Show enemies
+    for enemy in state.enemies.iter() {
+        enemy.show()
+    }
+
+    if state.is_game_over {
+        egui::Window::new("GAME OVER").show(egui(), |ui| ui.heading("GAME OVER!"));
+    }
+
+    egui::panel::TopBottomPanel::bottom("spreadsheet")
+        .exact_height(300.)
+        .show(egui(), |ui| {
+            ui.columns(2, |columns| {
+                let left_panel = &mut columns[0];
+
+                left_panel.heading("Player");
+                left_panel.separator();
+                left_panel.horizontal(|ui| {
+                    ui.label("Player HP: ");
+                    ui.add(
+                        ProgressBar::new(state.player.hp.remaining_fraction())
+                            .text(format!(
+                                "{} / {}",
+                                state.player.hp.current, state.player.hp.maximum
+                            ))
+                            .fill(RED.into()),
+                    )
+                });
+                left_panel.horizontal(|ui| {
+                    let pause_text = match state.is_paused {
+                        true => "Unpause",
+                        false => "Pause",
+                    };
+                    if ui.button(pause_text).clicked() {
+                        state.is_paused = !state.is_paused
+                    };
+                    if ui.button("Reset HP").clicked() {
+                        state.player.hp.reset();
+                    }
+                });
+
+                let right_panel = &mut columns[1];
+                right_panel.heading("Enemy spawner");
+                right_panel.separator();
+                right_panel.horizontal(|ui| {
+                    ui.label("Damage:");
+                    ui.add(
+                        egui::DragValue::new(&mut state.enemy_spawner.damage)
+                            .speed(1.0)
+                            .clamp_range(1. ..=100.),
+                    )
+                });
+                right_panel.horizontal(|ui| {
+                    ui.label("Speed:");
+                    ui.add(
+                        egui::DragValue::new(&mut state.enemy_spawner.speed)
+                            .speed(1.0)
+                            .clamp_range(1. ..=100.),
+                    )
+                });
+                right_panel.horizontal(|ui| {
+                    ui.label("Maximum HP:");
+                    ui.add(
+                        egui::DragValue::new(&mut state.enemy_spawner.maximum_hp)
+                            .speed(1.0)
+                            .clamp_range(1. ..=100.),
+                    )
+                });
+                right_panel.horizontal(|ui| {
+                    ui.label("Frequency (s):");
+                    let mut spawn_cooldown = state.enemy_spawner.timer.duration().as_secs_f32();
+                    ui.add(
+                        egui::DragValue::new(&mut spawn_cooldown)
+                            .speed(0.1)
+                            .clamp_range(0.1..=100.),
+                    );
+                    state
+                        .enemy_spawner
+                        .timer
+                        .set_duration(Duration::from_secs_f32(spawn_cooldown));
+                });
+            });
+        });
 }
---
+++ src/tile_map.rs
@@ -0,0 +1,120 @@
+use comfy::*;
+
+pub const TILE_SIZE: f32 = 1.; // This is in world coordinates
+pub const ROWS: u32 = 5;
+pub const COLUMNS: u32 = 28; // ZOOM is setup to 30, 1 margin on both sides
+
+pub fn draw() {
+    // TODO: 1/64 does not work well in general
+    let draw_grid_line = |p1, p2| draw_line(p1, p2, 1. / 64., DARKGRAY, 0);
+
+    let x_start = x_min();
+    let x_end = x_max();
+    for row in 0..=ROWS {
+        let y = y_into_absolute_start(row);
+        draw_grid_line(Vec2::new(x_start, y), Vec2::new(x_end, y))
+    }
+
+    let y_start = y_min();
+    let y_end = y_max();
+    for column in 0..=COLUMNS {
+        let x = x_into_absolute_start(column);
+        draw_grid_line(Vec2::new(x, y_start), Vec2::new(x, y_end))
+    }
+}
+
+pub struct TileMapPos {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub fn x_min() -> f32 {
+    main_camera().screen_top_left().x + TILE_SIZE // MARGIN
+}
+
+pub fn x_max() -> f32 {
+    x_min() + (COLUMNS as f32) * TILE_SIZE
+}
+
+pub fn x_from_absolute(x: f32) -> Option<u32> {
+    let x = (x - x_min()).div_euclid(TILE_SIZE);
+    if x >= 0. && x <= COLUMNS as f32 {
+        Some(x as u32)
+    } else {
+        None
+    }
+}
+
+pub fn x_into_absolute_start(x: u32) -> f32 {
+    x_min() + (x as f32) * TILE_SIZE
+}
+
+pub fn x_into_absolute_mid(x: u32) -> f32 {
+    x_into_absolute_start(x) + TILE_SIZE / 2.
+}
+
+pub fn x_into_absolute_end(x: u32) -> f32 {
+    x_into_absolute_start(x) + TILE_SIZE
+}
+
+pub fn y_max() -> f32 {
+    main_camera().screen_top_left().y - TILE_SIZE // margin
+}
+
+pub fn y_min() -> f32 {
+    y_max() - (ROWS as f32) * TILE_SIZE
+}
+
+pub fn y_into_absolute_start(y: u32) -> f32 {
+    y_min() + (y as f32) * TILE_SIZE
+}
+
+pub fn y_into_absolute_mid(y: u32) -> f32 {
+    y_into_absolute_start(y) + TILE_SIZE / 2.
+}
+
+pub fn y_into_absolute_end(y: u32) -> f32 {
+    y_into_absolute_start(y) + TILE_SIZE
+}
+
+pub fn y_from_absolute(y: f32) -> Option<u32> {
+    let y = (y - y_min()).div_euclid(TILE_SIZE);
+    if y >= 0. && y <= ROWS as f32 {
+        Some(y as u32)
+    } else {
+        None
+    }
+}
+
+impl TileMapPos {
+    pub fn new(x: u32, y: u32) -> TileMapPos {
+        Self { x, y }
+    }
+
+    pub fn from_absolute(pos: Vec2) -> Option<TileMapPos> {
+        let x = x_from_absolute(pos.x)?;
+        let y = y_from_absolute(pos.y)?;
+        Some(TileMapPos { x, y })
+    }
+
+    pub fn into_absolute_start(self) -> Vec2 {
+        Vec2 {
+            x: x_into_absolute_start(self.x),
+            y: y_into_absolute_start(self.y),
+        }
+    }
+
+    pub fn into_absolute_end(self) -> Vec2 {
+        Vec2 {
+            x: x_into_absolute_end(self.x),
+            y: y_into_absolute_end(self.y),
+        }
+    }
+
+    pub fn into_absolute_mid(self) -> Vec2 {
+        Vec2 {
+            x: x_into_absolute_mid(self.x),
+            y: y_into_absolute_mid(self.y),
+        }
+    }
+}
